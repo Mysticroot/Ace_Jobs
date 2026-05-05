f7dc4a742482e7bc307b13028ec8a1966d8fc9b7
Aashu <Aashupatil218@gmail.com>
2026-05-05 23:39:26 +0530

    coookie updated

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,7 +4,9 @@ const cors = require("cors");
 
 const app = express();
 
-const frontendOrigin = process.env.FRONTEND_URL || "http://localhost:5173";
+const frontendOrigin = (
+  process.env.FRONTEND_URL || "http://localhost:5173"
+).replace(/\/$/, "");
 
 app.use(express.json());
 app.use(cookieParser());
